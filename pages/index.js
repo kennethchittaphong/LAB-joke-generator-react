f4429652100d643ca3a5d3d34a781c99affe9348
Kenneth Chittaphong <kennethchittaphong@gmail.com>
2022-07-12 20:07:19 -0500

    got joke to generate

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,50 @@
+import React, { useState } from 'react';
+import getJoke from '../api/jokeData';
+
 function Home() {
+  const [btnText, setBtnText] = useState('GET A JOKE');
+
+  const [joke, setJoke] = useState({});
+
+  const setButton = (text) => {
+    setBtnText(text);
+  };
+
+  const getAJoke = () => {
+    getJoke().then((obj) => {
+      setJoke({
+        setup: obj.setup,
+        punchline: obj.delivery,
+      });
+
+      setButton('GET PUNCHLINE');
+    });
+  };
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Welcome Home!</h1>
-    </div>
+    <>
+      <h1>Joke Generator</h1>
+      <p>{joke.setup}</p>
+      {btnText !== 'GET PUNCHLINE' ? joke.punchline : ''}
+      {btnText === 'GET A JOKE' || btnText === 'GET A NEW JOKE' ? (
+        <button
+          className="btn btn-success"
+          type="button"
+          onClick={getAJoke}
+        >
+          {btnText}
+        </button>
+      ) : (
+        <button
+          onClick={() => setButton('GET A NEW JOKE')}
+          className="btn btn-success"
+          type="button"
+        >
+          {btnText}
+        </button>
+
+      )}
+    </>
   );
 }
 
